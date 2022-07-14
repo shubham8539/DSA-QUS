8034ae68c764d3dc28b90176fed96fcc50e1bb62
Your Name <kumarshubham853904@gmail.com>
2022-07-14 15:15:27 +0530

    commit

---
+++ swap.cpp
@@ -0,0 +1,36 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for(int i = 1; i<n; i++) {
+        //for round 1 to n-1
+        bool swapped = false;
+        
+        for(int j =0; j<n-i; j++) {
+            
+            //process element till n-i th index
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+            
+        }
+        
+        if(swapped == false) {
+            //already sorted
+            break;
+        }
+           
+    }
+}
+int main(){
+
+
+
+    vector<int> arr={34,56,44,33,1,0};
+    bubbleSort(arr,6);
+    for(int i=0;i<6;i++){
+        cout<<arr[i]<<" ";
+    }
+}
